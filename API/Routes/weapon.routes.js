5610a6c4d5a843425da9efba0cbaccedde829945
Gaston Manzano <gmanzano12@gmail.com>
2022-04-10 19:28:53 -0300

    -

--- API/Routes/weapon.routes.js
+++ API/Routes/weapon.routes.js
@@ -4,10 +4,10 @@ const router = Router();
 //controller
 const weaponController = require('../Controllers/weapon.controller');
 
-router.get('/weapon', weaponController.getAllWeapons);
-router.post('/weapon', weaponController.createWeapon);
-router.get('/weapon/:vocation', weaponController.getWeaponsByVocation);
-router.put('/weapon/:id', weaponController.editWeapon);
-router.delete('/weapon/:id', weaponController.deleteWeapon);
+router.get('/weapons', weaponController.getAllWeapons);
+router.post('/weapons', weaponController.createWeapon);
+router.get('/weapons/:vocation', weaponController.getWeaponsByVocation);
+router.put('/weapons/:id', weaponController.editWeapon);
+router.delete('/weapons/:id', weaponController.deleteWeapon);
 
 module.exports = router;
